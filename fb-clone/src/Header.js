c6751fc258833a2e94518d09da7e77d49dbe0a6f
cjriggs27 <cjriggs27@gmail.com>
2020-09-04 12:05:11 -0400

    Adding additional icons.

--- fb-clone/src/Header.js
+++ fb-clone/src/Header.js
@@ -2,11 +2,15 @@ import React from 'react';
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from "@material-ui/icons/Home";
-// import FlagIcon from "@material-ui/icons/Flag";
-// import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlinedIcon";
-// import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlinedIcon";
-// import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircleIcon";
-
+import FlagIcon from "@material-ui/icons/Flag";
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined";
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined";
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle";
+import { Avatar, IconButton } from '@material-ui/core';
+import AddIcon from "@material-ui/icons/Add";
+import ForumIcon from "@material-ui/icons/Forum";
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Header() {
     return (
@@ -27,9 +31,32 @@ function Header() {
                 <div className="header__option">
                     <HomeIcon fontSize="large"/>
                 </div>
+                <div className="header__option">
+                    <FlagIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    < SubscriptionsOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <StorefrontOutlinedIcon fontSize="large"/>
+                </div>
+                <div className="header__option">
+                    <SupervisedUserCircleIcon fontSize="large"/>
+                </div>
             </div>
             
-            <div className="header__right"></div>
+            <div className="header__right">
+                <div className="header__info">
+                    <Avatar  />
+                    <h4>Cody Jenkins</h4>
+                </div>
+
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+
+
+            </div>
             
         </div>
     )
