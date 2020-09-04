7260c5cd855f00610bc924453fe1be32b235aeb6
cjriggs27 <cjriggs27@gmail.com>
2020-09-04 15:01:35 -0400

    adding story reel

---
+++ fb-clone/src/StoryReel.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import "./StoryReel.css";
+import Story from './Story';
+
+function StoryReel() {
+    return (
+        <div className="storyReel">
+            <Story
+                image="https://cdn.shopify.com/s/files/1/0594/2893/articles/August-society-sustainable-swimwear-brazil-the-escape-artists1_73ecfc73-f08c-48b7-a530-ccd8fe265552_2048x.jpg?v=1557298275"
+                profileSrc="https://i.pinimg.com/originals/3f/31/23/3f3123a67ddb6a4b461c07e77a7d94b4.jpg"
+                title="Cody Jenkins"
+                
+            />
+
+            <Story
+                image="https://www.exoticca.net/blog/wp-content/uploads/2019/03/The-best-beaches-in-Brazil-Aren%E2%80%99t-you-going-to-visit-them.jpg"
+                profileSrc="https://www.southamerica.travel/wp-content/uploads/2019/08/brazil-beach-rainbow.jpg"
+                title="Johny Utah"
+                
+            />
+
+            <Story
+                image="https://www.cazloyd.com/sites/default/files/brazil-best-beach-destinations-to-visit.jpg"
+                profileSrc="https://cdn.cnn.com/cnnnext/dam/assets/130603180050-brazil-beaches-ipanema-beach.jpg"
+                title="Bodhi"
+                
+            />
+            
+            <Story
+                image="https://cdn.cnn.com/cnnnext/dam/assets/130605175231-brazil-beaches-taipus-de-fora.jpg"
+                profileSrc="https://webimages.trailfinders.com/image/fetch/dpr_auto,q_auto,f_auto,w_960,c_limit/https://images.trailfinders.com/asset/177863/TF1538146/Depositphotos_14373695_xl-2015_960x960.jpg"
+                title="Grommet"
+                
+            />
+
+            <Story
+                image="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2016%2F03%2Fcopacabana-beach-BRAZILBEACHES0316.jpg"
+                profileSrc="https://travel.home.sndimg.com/content/dam/images/travel/fullrights/2015/11/19/brazil-best-beaches/brazil-best-beaches-canoa.jpg.rend.hgtvcom.966.725.suffix/1491592861714.jpeg"
+                title="Angelo Pappas"
+         
+      />
+        </div>
+    )
+}
+
+
+export default StoryReel;
